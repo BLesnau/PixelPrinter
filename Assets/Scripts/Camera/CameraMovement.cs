81f0507018461e41faebcdc1a08c7f42acd5f2b2
Brett Lesnau <lesnaubr@gmail.com>
2016-02-14 23:25:38 -0500

    Rotate around Y only locally and start clamp code

--- Assets/Scripts/Camera/CameraMovement.cs
+++ Assets/Scripts/Camera/CameraMovement.cs
@@ -1,9 +1,20 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
+   [Serializable]
+   public class RotationClamps
+   {
+      public bool ClampRoation = false;
+      public float UpperClamp = 0;
+      public float LowerClamp = 0;
+   }
+
    public GameObject Target;
    public bool AutoLockRotation = false;
+   public bool LimitRotation = false;
+   public RotationClamps ClampRoation = new RotationClamps() {ClampRoation = false, UpperClamp = 0, LowerClamp = 0};
 
    private readonly ToggleEvent _startRotateXy = new ToggleEvent( MouseToggleEvent.LeftClick, TouchToggleEvent.OneFingerDown );
    private readonly ValueEvent _dragX = new ValueEvent( MouseValueEvent.XAxis, TouchValueEvent.OneFingerXAxis, 5, .5f );
@@ -33,13 +44,50 @@ public class CameraMovement : MonoBehaviour
             }
          }
 
-         var targetRot = Quaternion.AngleAxis( -rotX, this.transform.up );
-         targetRot *= Quaternion.AngleAxis( rotY, this.transform.right );
-
-         Target.transform.localRotation = targetRot * Target.transform.localRotation;
+         Quaternion targetRot;
+         if ( LimitRotation )
+         {
+            targetRot = Quaternion.AngleAxis( -rotX, Target.transform.up );
+            targetRot *= Quaternion.AngleAxis( rotY, this.transform.right );
+         }
+         else
+         {
+            targetRot = Quaternion.AngleAxis( -rotX, this.transform.up );
+            targetRot *= Quaternion.AngleAxis( rotY, this.transform.right );
+         }
+         targetRot = targetRot * Target.transform.localRotation;
+         if ( ClampRoation.ClampRoation )
+         {
+            var euler = targetRot.eulerAngles;
+            if ( !( ( euler.x < ClampRoation.UpperClamp ) || ( euler.x > 360 - ClampRoation.LowerClamp ) ) )
+            {
+               if ( Mathf.Abs( euler.x - ClampRoation.UpperClamp ) <= Mathf.Abs( euler.x - ClampRoation.LowerClamp ) )
+               {
+                  euler.x = ClampRoation.UpperClamp;
+               }
+               else
+               {
+                  euler.x = ClampRoation.LowerClamp;
+               }
+               targetRot = Quaternion.Euler( euler );
+            }
+         }
+         Target.transform.localRotation = targetRot;
       }
 
       var zoom = _zoom.GetValue();
       Target.transform.Translate( 0, 0, -zoom, Space.World );
+   }
+
+   public void ResetRotationY()
+   {
+      //var euler = Target.transform.localRotation.eulerAngles;
+      //euler.z = 0;
+      //Target.transform.localRotation = Quaternion.Euler( euler );
+      //Target.transform.Rotate( new Vector3( 10, 0, 0 ), Space.World );
+
+      var euler = Target.transform.localRotation.eulerAngles;
+      //Target.transform.localRotation = Quaternion.Euler( -euler.x, -euler.y, -euler.z );
+      Target.transform.localRotation = Quaternion.Euler( 0, euler.y, euler.z ) * Quaternion.LookRotation( Camera.main.transform.forward, Camera.main.transform.up );
    }
 }
