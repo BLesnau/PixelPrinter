d7ab5d15dc89060dc453afef043716884aa89f9f
Brett Lesnau <lesnaubr@gmail.com>
2016-02-27 15:33:25 -0500

    Initial 3 buttons on left with select box

---
+++ Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+   public Transform SelectBackground;
+
+   void Start()
+   {
+
+   }
+
+   void Update()
+   {
+   }
+
+   public void ButtonClicked( Transform trans )
+   {
+      SelectBackground.position = trans.position;
+   }
+}
