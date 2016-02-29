e19860a79dbad2cd8642ff6065116da97e907f28
Brett Lesnau <lesnaubr@gmail.com>
2016-02-28 23:13:31 -0500

    Setting tool state when buttons are pressed

---
+++ Assets/Scripts/UI/UIButton.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class UIButton : MonoBehaviour
+{
+   public UIManager UIManager;
+   public UIManager.Buttons ButtonType;
+
+   void Start()
+   {
+   }
+
+   void Update()
+   {
+   }
+
+   public void Click()
+   {
+      UIManager.ButtonClicked( transform, ButtonType );
+   }
+}
--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -5,17 +5,41 @@ public class UIManager : MonoBehaviour
 {
    public GameObject SelectBackground;
 
+   public enum Buttons { Add, Remove, Change }
+   public enum Tools { Add, Remove, Change }
+
+   public Tools SelectedTool;
+
    void Start()
    {
-
+      SelectedTool = Tools.Add;
    }
 
    void Update()
    {
    }
 
-   public void ButtonClicked( Transform trans )
+   public void ButtonClicked( Transform trans, Buttons button )
    {
+      switch ( button )
+      {
+         case Buttons.Add:
+         {
+            SelectedTool = Tools.Add;
+            break;
+         }
+         case Buttons.Remove:
+         {
+            SelectedTool = Tools.Remove;
+            break;
+         }
+         case Buttons.Change:
+         {
+            SelectedTool = Tools.Change;
+            break;
+         }
+      }
+
       var args = new Hashtable() { { "position", trans.position }, { "time", 1 } };
       iTween.MoveTo( SelectBackground, args );
    }
