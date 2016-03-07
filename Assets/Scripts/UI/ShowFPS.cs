6e9d509e878be505280d376b34822545bc18ba3a
Brett Lesnau <lesnaubr@gmail.com>
2016-03-06 23:13:11 -0500

    +FPS display

---
+++ Assets/Scripts/UI/ShowFPS.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShowFPS : MonoBehaviour
+{
+   private float deltaTime = 0.0f;
+
+   private void Update()
+   {
+      deltaTime += ( Time.deltaTime - deltaTime ) * 0.1f;
+
+      float msec = deltaTime * 1000.0f;
+      float fps = 1.0f / deltaTime;
+      string text = string.Format( "{0:0.0} ms ({1:0.} fps)", msec, fps );
+      DebugHelper.Log( "FPS", text );
+   }
+}
