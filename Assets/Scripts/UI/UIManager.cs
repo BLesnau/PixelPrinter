17e323c8a0ebe5887343c31b19180534a006d8b2
Brett Lesnau <lesnaubr@gmail.com>
2016-03-09 00:30:11 -0500

    First pass at color droplets - redo later

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -3,9 +3,10 @@ using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
-   public GameObject SelectBackground;
+   public GameObject ToolSelectBackground;
+   public GameObject ColorSelectBackground;
 
-   public enum Buttons { Add, Remove, Change }
+   public enum Buttons { Add, Remove, Change, Color1, Color2, Color3, Color4, Color5 }
    public enum Tools { Add, Remove, Change }
 
    public Tools SelectedTool;
@@ -21,26 +22,66 @@ public class UIManager : MonoBehaviour
 
    public void ButtonClicked( Transform trans, Buttons button )
    {
+      var toolClicked = false;
+      var colorClicked = false;
+
       switch ( button )
       {
          case Buttons.Add:
          {
             SelectedTool = Tools.Add;
+            toolClicked = true;
             break;
          }
          case Buttons.Remove:
          {
             SelectedTool = Tools.Remove;
+            toolClicked = true;
             break;
          }
          case Buttons.Change:
          {
             SelectedTool = Tools.Change;
+            toolClicked = true;
+            break;
+         }
+         case Buttons.Color1:
+         {
+            colorClicked = true;
+            break;
+         }
+         case Buttons.Color2:
+         {
+            colorClicked = true;
+            break;
+         }
+         case Buttons.Color3:
+         {
+            colorClicked = true;
+            break;
+         }
+         case Buttons.Color4:
+         {
+            colorClicked = true;
+            break;
+         }
+         case Buttons.Color5:
+         {
+            colorClicked = true;
             break;
          }
       }
 
-      var args = new Hashtable() { { "position", trans.position }, { "time", 1 } };
-      iTween.MoveTo( SelectBackground, args );
+      if ( toolClicked )
+      {
+         var args = new Hashtable() { { "position", trans.position }, { "time", 1 } };
+         iTween.MoveTo( ToolSelectBackground, args );
+      }
+
+      if ( colorClicked )
+      {
+         var args = new Hashtable() { { "position", trans.position }, { "time", 1 } };
+         iTween.MoveTo( ColorSelectBackground, args );
+      }
    }
 }
