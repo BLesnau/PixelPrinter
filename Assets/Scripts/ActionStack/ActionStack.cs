cc1ae5f5a16cf11cf6caae43e69445126276b90a
Brett Lesnau <Brett Lesnau>
2016-03-26 02:02:00 -0400

    +Basic functionality for ActionStack

--- Assets/Scripts/ActionStack/ActionStack.cs
+++ Assets/Scripts/ActionStack/ActionStack.cs
@@ -2,34 +2,53 @@
 
 public class ActionStack
 {
-   private LinkedList<IEditAction> _actions;
-   private LinkedList<IEditAction>.Enumerator _currentAction;
+   private readonly LinkedList<IEditAction> _actions;
+   private LinkedListNode<IEditAction> _currentAction;
 
    public ActionStack()
    {
       _actions = new LinkedList<IEditAction>();
-      _currentAction = _actions.GetEnumerator();
+      _currentAction = _actions.First;
    }
 
    public void AddAction( IEditAction action )
    {
+      _actions.RemoveAllAfter( _currentAction );
+
+      if ( _currentAction != null )
+      {
+         _currentAction = _actions.AddAfter( _currentAction, action );
+      }
+      else
+      {
+         _currentAction = _actions.AddFirst( action );
+      }
    }
 
    public bool CanUndo()
    {
-      return true;
+      return _currentAction != null;
    }
 
    public bool CanRedo()
    {
-      return true;
+      if ( _currentAction != null )
+      {
+         return _currentAction.Next != null;
+      }
+
+      return false;
    }
 
    public void Undo()
    {
+      _currentAction.Value.Undo();
+      _currentAction = _currentAction.Previous;
    }
 
    public void Redo()
    {
+      _currentAction = _currentAction.Next;
+      _currentAction.Value.Redo();
    }
 }
---
+++ Assets/Scripts/ExtensionMethods.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public static class ExtensionMethods
+{
+   public static void RemoveAllBefore<T>( this LinkedList<T> list, LinkedListNode<T> node )
+   {
+      if (node != null)
+      {
+         while (node.Previous != null)
+         {
+            list.Remove(node.Previous);
+         }
+      }
+   }
+
+   public static void RemoveAllAfter<T>( this LinkedList<T> list, LinkedListNode<T> node )
+   {
+      if ( node != null )
+      {
+         while ( node.Next != null )
+         {
+            list.Remove( node.Next );
+         }
+      }
+   }
+}
