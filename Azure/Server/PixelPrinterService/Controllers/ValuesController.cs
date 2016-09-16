ed45aaa8771e2aface66dea804e1ddcfb7241d94
Brett Lesnau <lesnaubr@gmail.com>
2016-09-15 23:26:12 -0400

    Formatting

--- Azure/Server/PixelPrinterService/Controllers/ValuesController.cs
+++ Azure/Server/PixelPrinterService/Controllers/ValuesController.cs
@@ -5,28 +5,28 @@ using Microsoft.Azure.Mobile.Server.Config;
 
 namespace PixelPrinterService.Controllers
 {
-    // Use the MobileAppController attribute for each ApiController you want to use  
-    // from your mobile clients 
-    [MobileAppController]
-    public class ValuesController : ApiController
-    {
-        // GET api/values
-        public string Get()
-        {
-            MobileAppSettingsDictionary settings = this.Configuration.GetMobileAppSettingsProvider().GetMobileAppSettings();
-            ITraceWriter traceWriter = this.Configuration.Services.GetTraceWriter();
+   // Use the MobileAppController attribute for each ApiController you want to use  
+   // from your mobile clients 
+   [MobileAppController]
+   public class ValuesController : ApiController
+   {
+      // GET api/values
+      public string Get()
+      {
+         MobileAppSettingsDictionary settings = this.Configuration.GetMobileAppSettingsProvider().GetMobileAppSettings();
+         ITraceWriter traceWriter = this.Configuration.Services.GetTraceWriter();
 
-            string host = settings.HostName ?? "localhost";
-            string greeting = "Hello from " + host;
-            
-            traceWriter.Info(greeting);
-            return greeting;
-        }
+         string host = settings.HostName ?? "localhost";
+         string greeting = "Hello from " + host;
 
-        // POST api/values
-        public string Post()
-        {
-            return "Hello World!";
-        }
-    }
+         traceWriter.Info( greeting );
+         return greeting;
+      }
+
+      // POST api/values
+      public string Post()
+      {
+         return "Hello World!";
+      }
+   }
 }
