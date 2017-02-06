25842eaf01a4997a874c46b135c5fb4ee2b32cfa
Brett Lesnau <lesnaubr@gmail.com>
2017-02-05 23:39:50 -0500

    No Login

--- Assets/Scripts/Azure/AzureHelper.cs
+++ Assets/Scripts/Azure/AzureHelper.cs
@@ -70,6 +70,8 @@ public class AzureHelper
 
    public static bool IsLoggedIn()
    {
+      return true;
+
       try
       {
          CheckTokenExpiration( SettingsManager.GetSetting( SettingsManager.StringSetting.AuthToken ) );
